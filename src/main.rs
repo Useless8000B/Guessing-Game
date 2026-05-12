8d7cb604d962679afd5e37d8e676303c6a647efe
Useless8000B <adrielluiz73@gmail.com>
2026-05-12 15:45:17 -0300

    Add basic verification

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,17 @@ fn main() {
 
         io::stdin()
             .read_line(&mut guess)
-            .expect("Error reading!");
+            .expect("Error reading guess!");
 
         let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue
         };
+
+        if guess < 1 || guess > 100 {
+            println!("The secret number will be between 1 and 100");
+        }
+
         println!("You guessed {guess}");
 
         match guess.cmp(&secret_number) {
